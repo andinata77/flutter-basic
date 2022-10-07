9901a02ad9ecb616827e5b04a8ba49f5ee1e446e
Andi_biasa_ajah <87589497+andiNata77@users.noreply.github.com>
2022-10-07 09:48:05 +0700

    Create column row stack

---
+++ cloumn_row_stack.dart
@@ -0,0 +1,45 @@
+COLUMN, ROW, STACK, MAINAXIS, CORSSAXIS
+
+import 'package:flutter/material.dart';
+
+main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Column"),
+        ),
+        body: Stack(
+          // mainAxisAlignment: MainAxisAlignment.center,
+          // crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            Container(
+              height: 400,
+              width: 400,
+              color: Colors.green,
+            ),
+            Container(
+              height: 300,
+              width: 300,
+              color: Colors.blue,
+            ),
+            Container(
+              height: 200,
+              width: 200,
+              color: Colors.amber,
+            ),
+            Container(
+              height: 100,
+              width: 100,
+              color: Colors.red,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
