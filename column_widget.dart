fe1c30ae75206bfca1306de67901d4ff7d078c6c
Andi_biasa_ajah <87589497+andiNata77@users.noreply.github.com>
2022-10-30 16:55:37 +0700

    Membuat Column Widget
    Belajar Membuat Column Widget

---
+++ column_widget.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Column"),
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            Container(
+              width: 200,
+              height: 50,
+              color: Colors.green,
+            ),
+            Container(
+              width: 50,
+              height: 50,
+              color: Colors.blue,
+            ),
+            Container(
+              width: 100,
+              height: 50,
+              color: Colors.amber,
+            ),
+            Container(
+              width: 300,
+              height: 50,
+              color: Colors.red,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
