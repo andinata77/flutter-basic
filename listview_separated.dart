927eaaf4df2712d256c1eb4ba5c314cb9c8d45dd
Andi_biasa_ajah <87589497+andiNata77@users.noreply.github.com>
2022-10-31 11:24:01 +0700

    Create ListView Separated
    Membuat ListView separated untuk membatasi dengan DIvider

---
+++ listview_separated.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  final List<Color> myColor = [
+    Colors.red,
+    Colors.green,
+    Colors.blue,
+    Colors.amber,
+    Colors.grey,
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("List View"),
+        ),
+        body: ListView.separated(
+          separatorBuilder: (contex, index) {
+            return Divider(
+              color: Colors.black,
+            );
+          },
+          itemCount: myColor.length,
+          itemBuilder: (context, index) {
+            return Container(
+              height: 300,
+              width: 300,
+              color: myColor[index],
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
