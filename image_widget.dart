e6a71a16bbff19265cd3af6e55094e82bfe2ffa0
Andi_biasa_ajah <87589497+andiNata77@users.noreply.github.com>
2022-10-31 17:13:09 +0700

    Update Image Widget
    Image Widget memiliki 4 akses : 
    1. Assets dari folder project kita -> mudah digunakan image sudah dipasang pada aplikasi
    sebelumnya didaftarkan pada pubspec.yaml agar dapat digunakan.
    2. Network atau link dari internet secara online -> sedikit berat saat run dan membutuhkan internet
    3. File dari folder lokal atau folder yang kita miliki -> sedikit lebih lambat dari assets namun mudah digunakan
    4. Memory menggunakan semacam unicode atau raw data-> ini jarang digunakan tapi lebih cepat

--- image_widget.dart
+++ image_widget.dart
@@ -1,28 +1,29 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text("Image Widget"),
-        ),
+        appBar: AppBar(title: Text('Image Widget')),
         body: Center(
           child: Container(
             width: 350,
             height: 500,
             color: Colors.amber,
             child: Image.asset(
-              "images/TTD_UPJ2.jpg",
+              'images/news1.jpg',
               fit: BoxFit.contain,
             ),
             // child: Image(
-            // fit: BoxFit.cover,
-            //   image: AssetImage("images/TTD_UPJ2.jpg"),
-            //   image: NetworkImage("https://picsum.photos/250/300?grayscale"),
+            //   fit: BoxFit.contain,
+            //   image: AssetImage('images/news1.jpg'),
+            //   image: NetworkImage("https://picsum.photos/300/300"),
+            // ),
           ),
         ),
       ),
