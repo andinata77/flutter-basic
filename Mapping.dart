f71330499c1dbb97ecbba54fe6630380e859e192
Andi_dart <87589497+andiNata77@users.noreply.github.com>
2022-11-16 17:16:11 +0700

    Update maping.dart
    Mengupdate maping dart.

--- Mapping.dart
+++ Mapping.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   final List<Map<String, dynamic>> myList = [
     {
-      "Name": "Andi",
+      "Name": "Andi Nata",
       "Age": 23,
       "favColor": [
         "Black",
@@ -16,16 +18,16 @@ class MyApp extends StatelessWidget {
         "Amber",
         "Black",
         "Red",
-        "Amber",
+        "Amber"
       ]
     },
     {
-      "Name": "Alex",
-      "Age": 25,
+      "Name": "Apri",
+      "Age": 20,
       "favColor": ["White", "Red", "Green"]
     },
     {
-      "Name": "Andi",
+      "Name": "Arya",
       "Age": 23,
       "favColor": [
         "Black",
@@ -36,11 +38,11 @@ class MyApp extends StatelessWidget {
         "Amber",
         "Black",
         "Red",
-        "Amber",
+        "Amber"
       ]
     },
     {
-      "Name": "Andi",
+      "Name": "Andi Nata",
       "Age": 23,
       "favColor": [
         "Black",
@@ -51,26 +53,16 @@ class MyApp extends StatelessWidget {
         "Amber",
         "Black",
         "Red",
-        "Amber",
+        "Amber"
       ]
     },
     {
-      "Name": "Andi",
-      "Age": 23,
-      "favColor": [
-        "Black",
-        "Red",
-        "Amber",
-        "Black",
-        "Red",
-        "Amber",
-        "Black",
-        "Red",
-        "Amber",
-      ]
+      "Name": "Apri",
+      "Age": 20,
+      "favColor": ["White", "Red", "Green"]
     },
     {
-      "Name": "Andi",
+      "Name": "Arya",
       "Age": 23,
       "favColor": [
         "Black",
@@ -81,113 +73,70 @@ class MyApp extends StatelessWidget {
         "Amber",
         "Black",
         "Red",
-        "Amber",
-      ]
-    },
-    {
-      "Name": "Andi",
-      "Age": 23,
-      "favColor": [
-        "Black",
-        "Red",
-        "Amber",
-        "Black",
-        "Red",
-        "Amber",
-        "Black",
-        "Red",
-        "Amber",
-      ]
-    },
-    {
-      "Name": "Andi",
-      "Age": 23,
-      "favColor": [
-        "Black",
-        "Red",
-        "Amber",
-        "Black",
-        "Red",
-        "Amber",
-        "Black",
-        "Red",
-        "Amber",
-      ]
-    },
-    {
-      "Name": "Andi",
-      "Age": 23,
-      "favColor": [
-        "Black",
-        "Red",
-        "Amber",
-        "Black",
-        "Red",
-        "Amber",
-        "Black",
-        "Red",
-        "Amber",
+        "Amber"
       ]
     },
   ];
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        appBar: AppBar(title: Text("ID Apps")),
-        body: ListView(
-          children: myList.map((data) {
-            print(data["favColor"]);
-            List myfavcolor = data["favColor"];
-            return Card(
-              margin: EdgeInsets.all(20),
-              color: Colors.black.withOpacity(0.1),
-              child: Container(
-                margin: EdgeInsets.all(10),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    //row
-                    Row(
-                      children: [
-                        CircleAvatar(),
-                        SizedBox(
-                          width: 20,
-                        ),
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text("Name: ${data['Name']}"),
-                            Text("Age: ${data['Age']}"),
-                          ],
-                        ),
-                      ],
-                    ),
-                    //favColor
-                    SingleChildScrollView(
-                      scrollDirection: Axis.horizontal,
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        children: myfavcolor.map((color) {
-                          return Container(
-                            color: Colors.amber,
-                            margin: EdgeInsets.symmetric(
-                              vertical: 15,
-                              horizontal: 8,
-                            ),
-                            padding: EdgeInsets.all(10),
-                            child: Text(color),
-                          );
-                        }).toList(),
-                      ),
-                    )
-                  ],
-                ),
-              ),
-            );
-          }).toList(),
+        appBar: AppBar(
+          title: Text('ID Apps'),
         ),
+        body: ListView(
+            children: myList.map((data) {
+          print(data["favColor"]);
+          List myfavcolor = data["favColor"];
+          print("---------");
+          print(myfavcolor);
+          return Card(
+            margin: EdgeInsets.all(20),
+            color: Colors.black.withOpacity(0.1),
+            child: Container(
+              margin: EdgeInsets.all(10),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  // row
+                  Row(
+                    children: [
+                      CircleAvatar(),
+                      SizedBox(
+                        width: 20,
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text("Name : ${data['Name']}"),
+                          Text("Age : ${data['Age']}"),
+                        ],
+                      ),
+                    ],
+                  ),
+                  //favColor
+                  SingleChildScrollView(
+                    scrollDirection: Axis.horizontal,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: myfavcolor.map((color) {
+                        return Container(
+                          color: Colors.amber,
+                          margin: EdgeInsets.symmetric(
+                            vertical: 15,
+                            horizontal: 8,
+                          ),
+                          padding: EdgeInsets.all(10),
+                          child: Text(color),
+                        );
+                      }).toList(),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          );
+        }).toList()),
       ),
     );
   }
