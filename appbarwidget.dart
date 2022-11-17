a64b431d90032e6f9bcd92c8d3fc71cb908495e9
Andi_dart <87589497+andiNata77@users.noreply.github.com>
2022-11-17 09:50:03 +0700

    Create appbarwidget.dart
    Membuat AppBarwidget agar lebih di custom

---
+++ appbarwidget.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          leading: Container(
+            color: Colors.amber,
+          ),
+          leadingWidth: 50,
+          title: Container(
+            // width: 35, akan menyesuaikan
+            height: 35,
+            color: Colors.red,
+          ),
+          // titleSpacing: 0,
+          centerTitle: false,
+          actions: [
+            Container(
+              width: 50,
+              // height: 10000,  akan menyesuaikan
+              color: Colors.purple,
+            ),
+            Container(
+              width: 50,
+              // height: 10000,  akan menyesuaikan
+              color: Colors.black,
+            ),
+            Container(
+              width: 50,
+              // height: 10000,  akan menyesuaikan
+              color: Colors.purple,
+            ),
+            Container(
+              width: 50,
+              // height: 10000,  akan menyesuaikan
+              color: Colors.black,
+            ),
+          ],
+          bottom: PreferredSize(
+            preferredSize: Size.fromHeight(100),
+            child: Container(
+              width: 50,
+              height: 50,
+              color: Colors.black,
+            ),
+          ),
+          flexibleSpace: Center(
+            child: Text(
+              "FLEXIBLE SPACE",
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 20,
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
