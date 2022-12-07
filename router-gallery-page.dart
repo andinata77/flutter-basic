a7e3bc740d53eda6e1ce6eb84c785e826f93e397
Andi_dart <87589497+andiNata77@users.noreply.github.com>
2022-12-07 10:46:01 +0700

    Create router-gallery-page.dart
    Ada 4 file
    - main.dart
      - gallery-page.dart
      - home-page.dart
      - photo-page.dart

---
+++ router-gallery-page.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import './photo-page.dart';
+
+class GalleryPage extends StatelessWidget {
+  static const nameRoute = '/gallerypage';
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Gallery Page"),
+        // leading: Container(),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Text(
+              "GALLERY PAGE",
+              style: TextStyle(
+                fontSize: 50,
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                TextButton(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                  child: Text(
+                    "<< Back",
+                    style: TextStyle(
+                      fontSize: 25,
+                    ),
+                  ),
+                ),
+                TextButton(
+                    onPressed: () {
+                      Navigator.of(context).pushNamed(PhotoPage.nameRoute);
+                    },
+                    child: Text(
+                      "Next >>",
+                      style: TextStyle(
+                        fontSize: 25,
+                      ),
+                    )),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
