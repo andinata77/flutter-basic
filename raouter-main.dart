9d5fab9dbe3eef304c212cab781fd7c305c31e88
Andi_dart <87589497+andiNata77@users.noreply.github.com>
2022-12-07 10:44:44 +0700

    Create raouter-main.dart
    Ada 4 file
    - main.dart
      - gallery-page.dart
      - home-page.dart
      - photo-page.dart

---
+++ raouter-main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import './pages/home-page.dart';
+import './pages/gallery-page.dart';
+import './pages/photo-page.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomePage(),
+      initialRoute: HomePage.nameRoute,
+      routes: {
+        HomePage.nameRoute: (context) => HomePage(),
+        GalleryPage.nameRoute: (context) => GalleryPage(),
+        PhotoPage.nameRoute: (context) => PhotoPage(),
+      },
+    );
+  }
+}
