e3e2908a87fbe036e6127b6aa00cd8b94ae4edf1
Andi_dart <87589497+andiNata77@users.noreply.github.com>
2023-01-03 17:40:19 +0700

    Create cart.dart

---
+++ multipe-provider/providers/cart.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+import '../models/cart_item.dart';
+
+class Cart with ChangeNotifier {
+  late Map<String, CartItem> _items = {};
+
+  Map<String, CartItem> get items => _items;
+
+  int get jumlah {
+    return _items.length;
+  }
+
+  double get totalHarga {
+    var total = 0.0;
+    _items.forEach((key, cartItem) {
+      total += cartItem.qty! * cartItem.price!;
+    });
+    return total;
+  }
+
+  void addCart(String productId, String title, double price) {
+    if (_items.containsKey(productId)) {
+      // ketika sudah tersedia key dari product id
+      _items.update(
+        productId,
+        (value) => CartItem(
+          id: value.id,
+          price: value.price,
+          qty: value.qty! + 1,
+          title: value.title,
+        ),
+      );
+    } else {
+      // nambah product baru
+      _items.putIfAbsent(
+        productId,
+        () => CartItem(
+          id: DateTime.now().toString(),
+          price: price,
+          qty: 1,
+          title: title,
+        ),
+      );
+    }
+    notifyListeners();
+  }
+}
