b6c5a975c2ab5932000d64dd843dc50114e7daab
Andi_dart <87589497+andiNata77@users.noreply.github.com>
2023-01-19 14:20:11 +0700

    Create main.dart

---
+++ wdiget-lifecycle/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+import './pages/counter_page.dart';
+import './pages/other_page.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: CounterPage(),
+      routes: {
+        CounterPage.routeName: (ctx) => CounterPage(),
+        OtherPage.routeName: (context) => OtherPage(),
+      },
+    );
+  }
+}
