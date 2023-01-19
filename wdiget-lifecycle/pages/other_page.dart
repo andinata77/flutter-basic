a5d6c115f896344988fce6009f1fc451ecabea1b
Andi_dart <87589497+andiNata77@users.noreply.github.com>
2023-01-19 14:21:30 +0700

    Create other_page.dart

---
+++ wdiget-lifecycle/pages/other_page.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+import './counter_page.dart';
+
+class OtherPage extends StatelessWidget {
+  static const routeName = "/other-page";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Other Page"),
+        ),
+        body: Center(
+          child: OutlinedButton(
+              onPressed: () {
+                Navigator.pushReplacementNamed(
+                  context,
+                  CounterPage.routeName,
+                  arguments: ModalRoute.of(context)!.settings.arguments as int,
+                );
+              },
+              child: Text("Go to Counter Page")),
+        ));
+  }
+}
