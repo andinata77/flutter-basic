b4ae6316b231f8d3182e9d37fc991b60079e2bd2
Andi_dart <87589497+andiNata77@users.noreply.github.com>
2023-01-19 17:37:15 +0700

    Update main.dart

--- app-lifecycle/main.dart
+++ app-lifecycle/main.dart
@@ -1,1 +1,83 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> with WidgetsBindingObserver {
+  int number = 0;
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addObserver(this);
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    print(state);
+    switch (state) {
+      case AppLifecycleState.inactive:
+        setState(() {
+          number = 99;
+        });
+        break;
+      case AppLifecycleState.paused:
+        // ....
+        break;
+      case AppLifecycleState.detached:
+        // ....
+        break;
+      case AppLifecycleState.resumed:
+        setState(() {
+          number = -20;
+        });
+        break;
+      default:
+    }
+    super.didChangeAppLifecycleState(state);
+  }
+
+  @override
+  void dispose() {
+    WidgetsBinding.instance.removeObserver(this);
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("COUNTER"),
+      ),
+      body: Center(
+        child: Text(
+          "$number",
+          style: TextStyle(fontSize: 35),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            number++;
+          });
+        },
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
 
