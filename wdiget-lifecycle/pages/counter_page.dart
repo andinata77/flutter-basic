306f07fa9eef508515fd11916ee5061b73ff8b60
Andi_dart <87589497+andiNata77@users.noreply.github.com>
2023-01-19 14:21:17 +0700

    Create counter_page.dart

---
+++ wdiget-lifecycle/pages/counter_page.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+import '../widgets/widget_counter.dart';
+
+class CounterPage extends StatefulWidget {
+  static const routeName = "/counter-page";
+  CounterPage() {
+    print("================");
+    print("Constructor Counter Page");
+  }
+  @override
+  _CounterPageState createState() {
+    return _CounterPageState();
+  }
+}
+
+class _CounterPageState extends State<CounterPage> {
+  int counter = 0;
+
+  @override
+  void didChangeDependencies() {
+    int numberState = ModalRoute.of(context)!.settings.arguments as int;
+    if (numberState != null) {
+      counter = numberState;
+    }
+
+    super.didChangeDependencies();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Counter Page"),
+      ),
+      body: WidgetCounter(counter: counter),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            counter++;
+          });
+        },
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
