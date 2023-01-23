016847cd2239a32fe8f0ac2b25a20a578b181ffc
Andi_dart <87589497+andiNata77@users.noreply.github.com>
2023-01-23 11:51:38 +0700

    Create main.dart

---
+++ nav-drawer/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import './nav-drawer.dart';
+
+void main() {
+  runApp(MaterialApp(
+    title: "BelajarFlutter.com",
+    home: BelajarNavigationDrawer(),
+  ));
+}
+
+class BelajarNavigationDrawer extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Belajar Drawer Nav"),
+      ),
+      drawer: DrawerWidget(),
+      endDrawer: DrawerWidget(),
+      body: Center(
+          child: Text('Belajar Navigation Drawer di Flutter',
+              style: TextStyle(fontSize: 20))),
+    );
+  }
+}
