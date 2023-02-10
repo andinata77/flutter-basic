b7031d3868185386411ee31efd18da378c30869f
Andi_dart <87589497+andiNata77@users.noreply.github.com>
2023-02-10 17:44:54 +0700

    Update login.dart
    selanjutnya kita akan membuat alert seperti SweatAlert dengan cara menambahkan plugin rflutter_alert, cara menambahkan nya cari file bernama pubspec.yaml
    Contoh:
    dependencies:
      flutter:
        sdk: flutter
      cupertino_icons: ^1.0.2
      rflutter_alert: ^2.0.2

--- login.dart
+++ login.dart
@@ -77,60 +77,74 @@ class _LoginPageState extends State<LoginPage> {
     );
   }
 
-  void submit(BuildContext context, String email, String password) {
-    if (email.isEmpty || password.isEmpty) {
-      final snackBar = SnackBar(
-        duration: Duration(seconds: 5),
-        content: Text("Email dan password harus diisi!"),
-        backgroundColor: Colors.red,
-      );
-      ScaffoldMessenger.of(context).showSnackBar(snackBar);
+//   void submit(BuildContext context, String email, String password) {
+//     if (email.isEmpty || password.isEmpty) {
+//       final snackBar = SnackBar(
+//         duration: Duration(seconds: 5),
+//         content: Text("Email dan password harus diisi!"),
+//         backgroundColor: Colors.red,
+//       );
+//       ScaffoldMessenger.of(context).showSnackBar(snackBar);
+//       return;
+//     }
+//     AlertDialog alert = AlertDialog(
+//       title: Text("Login Berhasil!"),
+//       content: Container(
+//         child: Text("Selamat Anda Berhasil Login"),
+//       ),
+//       actions: [
+//         TextButton(
+//           child: Text("Ok"),
+//           onPressed: () => Navigator.of(context).pop(),
+//         ),
+//       ],
+//     );
+//     showDialog(context: context, builder: (context) => alert);
+//     return;
+//   }
+// }
+
+  void sweatAlert(BuildContext context, String email, String password) {
+    if (email.isEmpty || password.isEmpty == null) {
+      Alert(
+        context: context,
+        type: AlertType.error,
+        title: "Login Gagal",
+        desc: "Email / Password salah!",
+        buttons: [
+          DialogButton(
+            child: Text(
+              "Selanjutnya",
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 14,
+              ),
+            ),
+            onPressed: () => Navigator.pop(context),
+          ),
+        ],
+      ).show();
+      return;
+    } else {
+      Alert(
+        context: context,
+        type: AlertType.success,
+        title: "Login Berhasil",
+        desc: "Selamat anda berhasil login",
+        buttons: [
+          DialogButton(
+            child: Text(
+              "Selanjutnya",
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 14,
+              ),
+            ),
+            onPressed: () => Navigator.pop(context),
+          ),
+        ],
+      ).show();
       return;
     }
-    AlertDialog alert = AlertDialog(
-      title: Text("Login Berhasil!"),
-      content: Container(
-        child: Text("Selamat Anda Berhasil Login"),
-      ),
-      actions: [
-        TextButton(
-          child: Text("Ok"),
-          onPressed: () => Navigator.of(context).pop(),
-        ),
-      ],
-    );
-    showDialog(context: context, builder: (context) => alert);
-    return;
   }
-}
-
-void sweatAlert(BuildContext context, String email, String password) {
-  if (email.isEmpty || password.isEmpty) {
-    final snackBar = SnackBar(
-      duration: Duration(seconds: 5),
-      content: Text("Email dan password harus diisi!"),
-      backgroundColor: Colors.red,
-    );
-    ScaffoldMessenger.of(context).showSnackBar(snackBar);
-    return;
-  }
-  Alert(
-    context: context,
-    type: AlertType.success,
-    title: "Login Berhasil",
-    desc: "Selamat anda berhasil login",
-    buttons: [
-      DialogButton(
-        child: Text(
-          "Selanjutnya",
-          style: TextStyle(
-            color: Colors.white,
-            fontSize: 14,
-          ),
-        ),
-        onPressed: () => Navigator.pop(context),
-      ),
-    ],
-  ).show();
-  return;
 }
