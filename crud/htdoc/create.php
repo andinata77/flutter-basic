51f9867eba21a682b295f18207c2ba805b740e6a
Andi_dart <87589497+andiNata77@users.noreply.github.com>
2023-02-16 10:48:10 +0700

    Create create.php

---
+++ crud/htdoc/create.php
@@ -0,0 +1,14 @@
+<?php
+$connection = new mysqli("localhost", "root", "", "db_crud_flutter");
+$title = $_POST['title'];
+$content = $_POST['content'];
+$date = $_POST['Y-m-d'];
+
+$result = mysqli_query($connection, "INSERT INTO tb_note_app set title ='$title', content='$content', date='$date'");
+
+if($result) {
+    echo json_encode(['message' => 'Data input successfully']);
+} else {
+    echo json_encode(['message' => 'Data Failed to input']);
+}
+?>
