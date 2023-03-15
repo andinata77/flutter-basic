7a2d3af69d5735748cc0cf2dd13620aebadfa7fe
Andi_dart <87589497+andiNata77@users.noreply.github.com>
2023-03-15 11:28:04 +0700

    Create auth_page.dart

---
+++ auth/lib2/pages/auth_page.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_login/flutter_login.dart';
+import 'package:provider/provider.dart';
+
+import '../providers/auth.dart';
+import './home_page.dart';
+
+const users = const {
+  'dribbble@gmail.com': '12345',
+  'hunter@gmail.com': 'hunter',
+};
+
+class LoginPage extends StatefulWidget {
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  Duration get loginTime => Duration(milliseconds: 2250);
+
+  Future<String?> _authUserSignUp(SignupData data) {
+    debugPrint('Signup Name: ${data.name}, Password: ${data.password}');
+    return Future.delayed(loginTime).then((_) {
+      Provider.of<Authh>(context, listen: false)
+          .signup(data.name, data.password);
+      return null;
+    });
+  }
+
+  Future<String?> _authLogin(LoginData data) {
+    debugPrint('Name: ${data.name}, Password: ${data.password}');
+    return Future.delayed(loginTime).then((_) {
+      Provider.of<Authh>(context, listen: false)
+          .login(data.name, data.password);
+      return null;
+    });
+  }
+
+  Future<String?> _authUser(LoginData data) {
+    debugPrint('Name: ${data.name}, Password: ${data.password}');
+    return Future.delayed(loginTime).then((_) {
+      if (!users.containsKey(data.name)) {
+        return 'User not exists';
+      }
+      if (users[data.name] != data.password) {
+        return 'Password does not match';
+      }
+      return null;
+    });
+  }
+
+  // Future<String?> _authUserSignUp(LoginData data) {
+  Future<String> _recoverPassword(String name) {
+    debugPrint('Name: $name');
+    return Future.delayed(loginTime).then((_) {
+      if (!users.containsKey(name)) {
+        return 'User not exists';
+      }
+      return null!;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FlutterLogin(
+      title: 'ECORP',
+      // logo: AssetImage('assets/images/ecorp-lightblue.png'),
+      onLogin: _authLogin,
+      onSignup: _authUserSignUp,
+      onSubmitAnimationCompleted: () {
+        Navigator.of(context).pushReplacement(MaterialPageRoute(
+          builder: (context) => HomePage(),
+        ));
+      },
+      onRecoverPassword: _recoverPassword,
+    );
+  }
+}
